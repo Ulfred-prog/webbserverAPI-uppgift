863564446ddc25357bcb83d43489c43be0466731
Ulfred-prog <alfred.sixtensson@skola.taby.se>
2026-02-17 13:12:27 +0100

    Add files via upload

---
+++ setup.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS api_db;
+USE api_db;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO users (username, password_hash) VALUES
+('uffe', '123'); 
